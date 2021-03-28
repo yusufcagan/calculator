392fc978904d8aca3a529a3e0247b87339438e28
Yusuf Cagan <yusufcagan1999@hotmail.com>
2021-03-28 23:19:09 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text ,View} from 'react-native'
+
+function App () {  
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+
+export default App;
