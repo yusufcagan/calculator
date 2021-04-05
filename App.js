183b74de64dff23fd67136a7ae5ded2e39899fab
Yusuf Cagan <yusufcagan1999@hotmail.com>
2021-04-05 18:16:40 +0300

    commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React, {useState, useRef,useEffect} from 'react'
-import {Text ,View, StyleSheet, Dimensions, TouchableOpacity, Input, TextInput, Keyboard, Alert} from 'react-native'
+import React, {useState} from 'react'
+import {Text ,View, StyleSheet, Dimensions, TouchableOpacity, TextInput, Alert} from 'react-native'
 
-const {width,height} =Dimensions.get('window');
+const {width, height} =Dimensions.get('window');
 
 
 function App () {
@@ -15,31 +15,52 @@ function App () {
  
   const onChange = (cNumber) => {
 
-    if(cNumber == '+' || cNumber == '=' || cNumber == '-' ){
+    if(cNumber == '+' || cNumber == '=' || cNumber == '-' || cNumber == '/' || cNumber == 'x' || cNumber == '%' || cNumber == 'del'){
       
-      if(bool2==true){
+      if(bool2 == true){
           switch (temp) {
           case '+':
-            setNumber(sum1+parseInt(sum2))
+            setNumber(sum1+parseFloat(sum2))
             break;
           
           case '-':
-            setNumber(sum1-parseInt(sum2))
+            setNumber(sum1-parseFloat(sum2))
             break;
-      
+          
+          case '/':
+            setNumber(sum1/parseFloat(sum2))
+            break;
+          
+          case 'x':
+            setNumber(sum1*parseFloat(sum2))
+            break;
+
+          
           default:
             break;
          }
-         if(cNumber=='='){
-           setbool(false);
-           setbool2(false);
-           setsum1('');
-           setsum2('');
+         
+         if(cNumber == '=' || cNumber == '-' || cNumber == '/' || cNumber == 'x' || cNumber == '+'){
+           setbool(false)
+           setbool2(false)
+           setsum1('')
+           setsum2('')
            //setNumber(Number.toString());
            return Number;
          }
       }
-      
+      if(cNumber == '%'){
+        setNumber(parseFloat(Number)/100)
+        setbool(false)
+        setbool2(false)
+        setsum1('')  
+        return Number;
+      }
+      if(cNumber == 'del'){
+        const del = Number.slice(0, -1)
+        setNumber(del)
+        return Number;
+      }
 
       settemp(cNumber);
       setbool(true);
@@ -47,34 +68,32 @@ function App () {
     }
 
     if(bool==true){
-      setsum1(parseInt(Number));
-      setbool(false);
+      setsum1(parseFloat(Number))
+      setbool(false)
     }
-    setNumber(Number+cNumber);
+    setNumber(Number+cNumber)
     if(bool2==true){
-      setsum2(sum2+cNumber);
+      setsum2(sum2+cNumber)
     }
     
   };
-    //Alert.alert(Number);
-    //console.log(typeof(temp));
-    //console.log(temp);
 
   return (
     <View style={{flex:1,backgroundColor:'#000'}}>         
       <View style={{height:'43%', justifyContent:'flex-end', alignItems:'flex-end'}}><TextInput showSoftInputOnFocus={false} style={{fontSize:60, color:'white'}}>{Number}</TextInput></View>
       <View style={{flexDirection:'row', width:width, justifyContent:'space-between'}}>
         <TouchableOpacity onPress={() => setNumber('')}><View style={styles.border}><Text style={{fontSize:40, color:'#b32400'}}>C</Text></View></TouchableOpacity>
-        <TouchableOpacity onPress={() => onChange('(')}><View style={styles.border}><Text style={styles.cl}>( )</Text></View></TouchableOpacity>
-        <TouchableOpacity onPress={() => onChange('%')}><View style={styles.border}><Text style={styles.cl}>%</Text></View></TouchableOpacity>
+        <TouchableOpacity onPress={() => onChange('x')}><View style={styles.border}><Text style={styles.cl}>X</Text></View></TouchableOpacity>
         <TouchableOpacity onPress={() => onChange('/')}><View style={styles.border}><Text style={styles.cl}>/</Text></View></TouchableOpacity>
+        <TouchableOpacity onPress={() => onChange('del')}><View style={styles.border}><Text style={{fontSize:30, color:'#b32400'}}>DEL</Text></View></TouchableOpacity>    
+                
       </View>   
 
       <View style={{flexDirection:'row', width:width, justifyContent:'space-between'}}>
         <TouchableOpacity onPress={() => onChange('7')}><View style={styles.border}><Text style={styles.text}>7</Text></View></TouchableOpacity>
         <TouchableOpacity onPress={() => onChange('8')}><View style={styles.border}><Text style={styles.text}>8</Text></View></TouchableOpacity>
         <TouchableOpacity onPress={() => onChange('9')}><View style={styles.border}><Text style={styles.text}>9</Text></View></TouchableOpacity>
-        <TouchableOpacity onPress={() => onChange('x')}><View style={styles.border}><Text style={styles.cl}>X</Text></View></TouchableOpacity>
+        <TouchableOpacity onPress={() => onChange('%')}><View style={styles.border}><Text style={styles.cl}>%</Text></View></TouchableOpacity>
       </View> 
 
       <View style={{flexDirection:'row', width:width, justifyContent:'space-between'}}>
@@ -94,7 +113,7 @@ function App () {
       <View style={{flexDirection:'row', width:width, justifyContent:'space-between'}}>
         <TouchableOpacity onPress={() => setNumber(Number*-1)}><View style={styles.border}><Text style={styles.text}>+/-</Text></View></TouchableOpacity>
         <TouchableOpacity onPress={() => onChange('0')}><View style={styles.border}><Text style={styles.text}>0</Text></View></TouchableOpacity>
-        <TouchableOpacity onPress={() => onChange(',')}><View style={styles.border}><Text style={styles.text}>,</Text></View></TouchableOpacity>
+        <TouchableOpacity onPress={() => onChange('.')}><View style={styles.border}><Text style={styles.text}>,</Text></View></TouchableOpacity>
         <TouchableOpacity onPress={() => onChange('=')}><View style={styles.equal}><Text style={styles.text}>=</Text></View></TouchableOpacity>
       </View>    
 
